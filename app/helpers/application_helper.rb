41b136b137c72af70df87938c1525fbd1ecfee15
Vinay KP <vinay.kp@jifflenow.com>
2018-07-25 23:50:45 +0530

    KP : display color added info with title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	def generate_grid(row,col)
     color_data = fetch_color_info(row, col)
     raw("<div class='content' onclick='update_grid_color(this);'
-         id='grid-#{row}_#{col}' style= 'background-color: #{color_data[0]}'></div>")
+         id='grid-#{row}_#{col}' style= 'background-color: #{color_data[0]}' title='#{color_data[1]}'></div>")
   end
 
   def fetch_color_info(row, col)
